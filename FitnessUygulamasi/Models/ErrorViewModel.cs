de34c239808eda1f78ae6d1d0e2c77632107d527
yusuf <yusuf@MELIODAS>
2025-12-19 18:25:08 +0300

    Add project files.

---
+++ FitnessUygulamasi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FitnessUygulamasi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

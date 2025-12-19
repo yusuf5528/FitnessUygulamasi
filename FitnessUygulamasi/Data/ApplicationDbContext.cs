05438a26900c5bb60076e71055814611c2ff360d
yusuf <yusuf@MELIODAS>
2025-12-19 21:12:47 +0300

    Configured DbContext and connection string

---
+++ FitnessUygulamasi/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using FitnessUygulamasi.Models;
+
+namespace FitnessUygulamasi.Data
+{
+    // IdentityDbContext kullanarak üyelik tablolarını otomatik getireceğiz
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
